fd86eae886058be673fe827dea55bfc8af466d4a
Saga Eriksson <sagaemilia000@gmail.com>
2025-06-03 10:32:25 +0200

    Finised auth

---
+++ models/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        unique : true,
+        minlength : 5,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true,
+        minlength : 8
+    },
+    userId : {
+        type : String,
+        required : true,
+        unique : true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
